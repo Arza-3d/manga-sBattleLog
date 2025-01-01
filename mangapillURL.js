ffc585a46ccbefbad62a819464ac66e219bc5ae4
Forest Guhareza <arza.3d@gmail.com>
2025-01-01 11:38:11 +0700

    update mangapillURL.js

--- mangapillURL.js
+++ mangapillURL.js
@@ -17,17 +17,3 @@ function generateMangaPillChapterUrl(chapterNumber) {
 
   return fullUrl;
 }
-
-// Example usage
-try {
-  const url1 = generateMangaPillChapterUrl(5);
-  console.log(url1); // Output: https://mangapill.com/chapters/3069-10005000/naruto-chapter-5
-
-  const url2 = generateMangaPillChapterUrl(30);
-  console.log(url2); // Output: https://mangapill.com/chapters/3069-10030000/naruto-chapter-30
-
-  const url3 = generateMangaPillChapterUrl(697);
-  console.log(url3); // Output: https://mangapill.com/chapters/3069-10697000/naruto-chapter-697
-} catch (error) {
-  console.error(error.message);
-}
